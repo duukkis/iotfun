3bf16fac714ab5df974e3914dcaed7f27bb522dd
Duukkis <duukkis@hotmail.com>
2019-03-18 19:07:32 +0200

    Update test.ino

--- oled/test.ino
+++ oled/test.ino
@@ -6,13 +6,7 @@
 // SCL GPIO5
 // SDA GPIO4
 #define OLED_RESET 0  // GPIO0
-Adafruit_SSD1306 display(OLED_RESET);
- 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
- 
+Adafruit_SSD1306 display(OLED_RESET); 
  
 #define LOGO16_GLCD_HEIGHT 16
 #define LOGO16_GLCD_WIDTH  16
